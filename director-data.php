2c0523995efed8de8ce2c10cf93cfc756aa4e3b8
CrackerCrypto <rajshybal10@gmail.com>
2022-05-30 15:00:42 +0530

    actor+director

--- director-data.php
+++ director-data.php
@@ -1,19 +1,19 @@
-<?php include_once "connection.php";?>
+<?php include_once "connection.php"; ?>
 <?php
     $name = $_POST['name'];
     $dob = $_POST['dob'];
     // Cheecking whether the data is already on the table or not
-    $sql = "SELECT * FROM `actor` WHERE `actor_name` = '$name'";
+    $sql = "SELECT * FROM `director` WHERE `director_name` = '$name'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
-    if((mysqli_num_rows($result)>=1) && ($dob==$row['actor_dob'])){
+    if((mysqli_num_rows($result)>=1) && ($dob==$row['director_dob'])){
         echo "Data is already on the database";
         mysqli_free_result($result);
         die();
     }
     else{
         // Insert tha data
-        $sql = "INSERT INTO `actor`(`actor_name`, `actor_dob`)
+        $sql = "INSERT INTO `director`(`director_name`, `director_dob`)
                     VALUES('$name', '$dob')";
         $result = mysqli_query($conn, $sql);
         if($result){
