c0571bb41f63192258e551112c689a483a19100a
CrackerCrypto <rajshybal10@gmail.com>
2022-06-03 23:25:50 +0530

    actor almost done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MovieDB
+1. Make a suitable front-end using React
+    1.1. Learn the component based and try to implement using tailwind css
+2. Complete the remaining back-end task for(director/director_movie/director_actor/quotes)
--- actor_details.php
+++ actor_details.php
@@ -1,26 +1,38 @@
 <?php include_once "connection.php"; ?>
 <?php
     echo "<h1>Actor Details page</h1>";
-    $actor_name = $_POST['actor_name'];
+    $actor = $_POST['actor_name'];
 
-    $sql = "SELECT * FROM `actor` WHERE `actor_name` = '$actor_name'";
-    $res = mysqli_query($conn, $res);
-    if((mysqli_num_rows($result)!=1)){
-        echo "Actor is not present in table";
-        mysqli_free_result($res);
-        die();
-    }
-    echo "<table border='1'>
-    <tr>
-        <th>Actor Name:</th>
-        <th>Genre</th>
-    </tr>";
-
-    while($row = mysqli_fetch_assoc($r)){
+    $val = mysqli_query($conn, "SELECT * FROM `actor` WHERE `actor_name` = '$actor'");
+    if(mysqli_num_rows($val)>=1){
+        $sql = "SELECT * FROM `actor_movie` WHERE `actor_name` = '$actor'";
+        $val1 = mysqli_query($conn, $sql);
+        $numrow = mysqli_num_rows($val1);
+        echo "<table border = '1'>
+        <tr>
+            <th colspan='2'>Actor's Details</th>
+        </tr>";
         echo "<tr>";
-            // echo "<td>" .$row['m_title']."</td>";
-            // echo "<td>" .$row['genre']."</td>";
+            echo "<td>Actor Name</td>";
+            echo "<td><b>".$actor."</b></td>";
         echo "</tr>";
+        echo "<tr>";
+            echo "<td>Number of Movies</td>";
+            echo "<td><b>".$numrow."</b></td>";
+        echo "</tr>";
+        echo "<tr>";
+            echo "<td>Moive List</td>";
+                echo "<td><b>";
+                    while($list = mysqli_fetch_assoc($val1)){
+                        echo $list['m_name'];
+                        echo "<br>";
+                    }
+                echo "</b></td>"; 
+        echo "</tr>";
+        echo "</table>";
+
     }
-    echo "</table>";
+    else{
+        echo $actor." is not present in the table";
+    }
 ?>
